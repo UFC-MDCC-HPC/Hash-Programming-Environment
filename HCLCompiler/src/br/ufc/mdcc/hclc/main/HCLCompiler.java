f460376002dfae867c9a0b9d9fa971d0a59ed575
carvalho.heron <carvalho.heron@574a1136-cea5-11dd-b6f5-21ad4398c266>
2010-04-30 03:46:52 +0000

    Adding a main class.

--- HCLCompiler/src/br/ufc/mdcc/hclc/main/HCLCompiler.java
+++ HCLCompiler/src/br/ufc/mdcc/hclc/main/HCLCompiler.java
@@ -3,6 +3,16 @@
  */
 package br.ufc.mdcc.hclc.main;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import br.ufc.mdcc.hclc.parser.HCLParser;
+import br.ufc.mdcc.hclc.parser.ParseException;
+
 /**
  * @author Heron
  *
@@ -11,10 +21,41 @@ public final class HCLCompiler {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-
+        System.out.println("The HCL Compiler");
+        
+        FileInputStream file = null;
+        InputStreamReader in = null;
+        
+		try {
+    	    file = new FileInputStream("h:\\temp\\teste.hcl");
+    	    
+    	    in = new InputStreamReader(file, "UTF-8");
+        
+		    HCLParser parser = new HCLParser(in);
+			
+			parser.Start();
+			
+		} catch (ParseException e) {
+			
+			e.printStackTrace();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			in.close();
+			file.close();
+		}
+		
+		
+		
 	}
 
 }
