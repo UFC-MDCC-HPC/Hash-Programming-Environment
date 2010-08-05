b0faf9c5b3bcca777b1b53044647509012ea7f40
carvalho.heron <carvalho.heron@574a1136-cea5-11dd-b6f5-21ad4398c266>
2010-08-05 20:45:25 +0000

    essage

--- HPE_BackEnd/DGAC/ServerObject.cs
+++ HPE_BackEnd/DGAC/ServerObject.cs
@@ -5,6 +5,7 @@ using System.Runtime.Remoting;
 using System.Collections.Generic;
 using DGAC.utils;
 using MPI;
+// using cca;
 
 namespace DGAC
 { 
@@ -14,11 +15,26 @@ namespace DGAC
             private int my_rank = -1;
 
             private MPI.Intracommunicator global_communicator = null;
-
+ 
+    	    /**
+	     * 	Creates an instance of a CCA component of the type defined by the 
+	     * 	string className.  The string classname uniquely defines the
+	     * 	"type" of the component, e.g.
+	     * 	    doe.cca.Library.GaussianElmination. 
+	     * 	It has an instance name given by the string instanceName.
+	     * 	The instanceName may be empty (zero length) in which case
+	     * 	the instanceName will be assigned to the component automatically.
+	     * 	@throws CCAException If the Component className is unknown, or if the
+	     * 		instanceName has already been used, a CCAException is thrown.
+	     * 	@return A ComponentID corresponding to the created component. Destroying
+	     * 		the returned ID does not destroy the component; 
+	     * 		see destroyInstance instead.
+	     */
             [MethodImpl(MethodImplOptions.Synchronized)]
-            public void createInstance(int[] nodes)
+            public void createInstance(string instanceName, string className /*, cca.TypeMap properties */)
             {
              try {
+                int[] nodes = new int[] {1,2,3,4};
                 Console.WriteLine("CREATE INSTANCE" + nodes.Length);
 
                 this.global_communicator = MPI.Communicator.world; 
