867dcbc5154f9034f0767d821f570766bc08af1f
carvalho.heron <carvalho.heron@574a1136-cea5-11dd-b6f5-21ad4398c266>
2010-09-20 21:46:39 +0000

    adding facets Domain e Facet no Back-End

---
+++ HPE_BackEnd/DGAC/base/DomainKind.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using br.ufc.hpe.basic;
+
+namespace br.ufc.hpe.kinds
+{
+
+
+    public interface IDomainKind : IUnit
+    {
+    }
+
+    public abstract class Domain : Unit
+    {
+
+    }
+
+}
---
+++ HPE_BackEnd/DGAC/base/FacetKind.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using br.ufc.hpe.basic;
+
+namespace br.ufc.hpe.kinds
+{
+
+
+    public interface IFacetKind : IUnit
+    {
+    }
+
+    public abstract class Facet : Unit
+    {
+
+    }
+
+}
