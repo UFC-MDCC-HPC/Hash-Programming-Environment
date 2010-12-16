f12ac3fec1090a34fae3b8874110994dddd330db
carvalho.heron <carvalho.heron@574a1136-cea5-11dd-b6f5-21ad4398c266>
2010-12-16 13:51:47 +0000

    removing Linq dependency

--- HPE_BackEnd/ws_repository/ComponentRepository.asmx.cs
+++ HPE_BackEnd/ws_repository/ComponentRepository.asmx.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.ComponentModel;
 using System.Data;
-using System.Linq;
+//using System.Linq;
 using System.Web;
 using System.Web.Services;
 using System.Web.Services.Protocols;
