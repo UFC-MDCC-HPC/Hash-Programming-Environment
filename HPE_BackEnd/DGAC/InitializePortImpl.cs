fd3e2b19c8ef7b5931e1bf707c5eabeb62d0b93a
carvalho.heron <carvalho.heron@574a1136-cea5-11dd-b6f5-21ad4398c266>
2012-12-10 11:11:06 +0000

    InitializePortImpl

---
+++ HPE_BackEnd/DGAC/InitializePortImpl.cs
@@ -0,0 +1,126 @@
+using System;
+using gov.cca.ports;
+using gov.cca;
+using br.ufc.pargo.hpe.backend.DGAC;
+using System.Collections.Generic;
+using System.Threading;
+using br.ufc.pargo.hpe.backend.DGAC.utils;
+using br.ufc.pargo.hpe.ports;
+
+namespace br.ufc.hpe.backend.DGAC
+{
+	public class InitializePortImpl : InitializePort
+	{
+		private ManagerComponentID mcid = null;
+		private string session_id_string = null;
+		private Port[] w_initialize_port = null;
+		
+		public InitializePortImpl(ManagerServices services, Port[] winitialize_ports)
+		{
+		   this.mcid = (ManagerComponentID) services.getComponentID();
+		   this.session_id_string = mcid.getInstanceName();
+		   this.w_initialize_port = winitialize_ports;
+		}
+
+		#region AutomaticSlicesPort implementation
+		public void initialize ()
+		{
+			
+            IDictionary<Thread, InitializeThread> thread_list = new Dictionary<Thread,InitializeThread>();
+            for (int i=0; i<w_initialize_port.Length; i++)
+            {		       
+				InitializeThread thread = new InitializeThread((InitializePort) w_initialize_port[i]);
+                Thread t = new Thread(thread.Run);
+                thread_list.Add(t,thread);
+                t.Start();
+            }
+            foreach (KeyValuePair<Thread,InitializeThread> t in thread_list)
+            {
+                try 
+                {
+					t.Key.Join();
+                   Console.Error.WriteLine("Worker thread arrived : " + session_id_string);
+                  // outputs[t.Value.Node] = t.Value.Output;
+                } 
+                catch (Exception e)
+                {
+                   Console.WriteLine("Worker failed : " + session_id_string + ". error =" + e.Message);
+                }
+            }			
+            Console.Error.WriteLine("Joined Threads : " + session_id_string);
+            
+			
+		}
+		#endregion
+
+        internal class InitializeThread
+        {
+			private InitializePort worker_initialize_port;
+
+            public InitializeThread(InitializePort worker_initialize_port)
+            {
+				this.worker_initialize_port = worker_initialize_port;
+            }
+
+            public void Run()
+            {
+				try {
+					worker_initialize_port.initialize();				
+				}
+				catch (Exception e)
+				{
+					throw e;
+				}
+            }
+
+        }
+
+		#region AutomaticSlicesPort implementation
+		public void post_initialize ()
+		{
+			
+            IDictionary<Thread, AfterInitializeThread> thread_list = new Dictionary<Thread,AfterInitializeThread>();
+            for (int i=0; i<w_initialize_port.Length; i++)
+            {		       
+				AfterInitializeThread thread = new AfterInitializeThread((InitializePort) w_initialize_port[i]);
+                Thread t = new Thread(thread.Run);
+                thread_list.Add(t,thread);
+                t.Start();
+            }
+            foreach (KeyValuePair<Thread,AfterInitializeThread> t in thread_list)
+            {
+                try 
+                {
+					t.Key.Join();
+                   Console.Error.WriteLine("Worker thread arrived : " + session_id_string);
+                  // outputs[t.Value.Node] = t.Value.Output;
+                } 
+                catch (Exception e)
+                {
+                   Console.WriteLine("Worker failed : " + session_id_string + ". error =" + e.Message);
+                }
+            }			
+            Console.Error.WriteLine("Joined Threads : " + session_id_string);
+		}
+		#endregion
+
+        internal class AfterInitializeThread
+        {
+			private InitializePort worker_initialize_port;
+
+            public AfterInitializeThread(InitializePort worker_initialize_port)
+            {
+				this.worker_initialize_port = worker_initialize_port;
+            }
+
+            public void Run()
+            {
+				worker_initialize_port.post_initialize();				
+            }
+
+        }
+
+	}
+
+}
+
